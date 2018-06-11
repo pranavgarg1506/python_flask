9745c35e9997408e751058644a7f8ba05fdcaaca
pranav garg <pranavfunny1@gmail.com>
2018-06-11 12:11:32 +0530

    update2

--- project1/sample.py
+++ project1/sample.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-from  flask import Flask,render_template,request
+# USE oF {{}}??
+
+from  flask import Flask,render_template,request,redirect,url_for
 
 # defining app 
 app=Flask(__name__)
@@ -11,41 +13,54 @@ page222 = '''<h1> lol </h1>'''
 page1 = '''<h1> hiiii </h1>'''
 
 
-
-
-
 # start route
 @app.route('/')
-
 # below function is under the app object and only one function can be defined under one object
 def webpage():	
 	return page222
 
 
-
 @app.route('/lol')
-
 def lol():
 	return page1
 
 
 
 @app.route('/cal')
-
 def cal():
 	x=10
 	y=20
 	return str(x+y)
 
-@app.route('/list1')
 
-def list1():
+@app.route('/list1')
+def list1():		
 	list1 = ['1','2']
 	return list1[0]
 
-@app.route('/quiz')
+
+@app.route('/quiz',methods=['GET','POST'])
 def page():
-	return render_template('quiz.html')
+	if request.method == 'POST':
+		return render_template('quiz.html')
+	else :
+		return render_template('a.html')
+
+
+# to pass a variable
+@app.route('/var<name>')	# <float:hi>  <int:id1>
+def var_pass(name):
+	return 'Hello!! %s(GUEST)' % name #  %f hi	%d id1
+
+
+
+# to pass diff parameters
+@app.route('/usr<name>')
+def user(name):
+	if name == 'admin':
+		return 'Hello %s' % name	# here we can also use any othe app.route for defining admin fn
+	else :
+		return redirect(url_for('var_pass',name = name))
 
 
 
@@ -56,3 +71,9 @@ def page():
 if __name__  ==  "__main__" :
 	app.run(debug=True,host='192.168.43.168')
 		# if we define 2 diff objects the it will first load the first object then (ctrl + c) to close the connnection and it will load the next object if there is...
+
+
+
+
+
+
